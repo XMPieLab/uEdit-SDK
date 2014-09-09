ae95fc4e772a7c1852bfe8d0f74afe143435e5ff
gal kahana <gal.kahana@hotmail.com>
2014-09-09 14:28:14 +0300

    basic uEdit setup materials

---
+++ uEdit Setup/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+	$('#uedit-control').UEdit({
+                                width:700, 
+                                height:500
+                            });
+
+});
